a8225abbd3770420dd4a1ca67946b485c62ea7d7
jmsnuwan-dotcom <jmsnuwan@gmail.com>
2026-08-07 18:52:27 +0530

    Add storage folder

--- server/database/db.py
+++ server/database/db.py
@@ -2,12 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from server.config import DATABASE_URL
-
+from server.config import BASE_DIR, DATABASE_URL
+from pathlib import Path
 
 # ---------------------------------------
 # Database Engine
 # ---------------------------------------
+
+db_path = Path(BASE_DIR) / "storage"
+db_path.mkdir(parents=True, exist_ok=True)
 
 engine = create_engine(
     DATABASE_URL,
