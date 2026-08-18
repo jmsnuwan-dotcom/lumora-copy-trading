e7d02ab4c21ffad05ba70873fbe10ed39c3233d1
jmsnuwan-dotcom <jmsnuwan@gmail.com>
2026-08-18 20:23:21 +0530

    Add connection balance equity migration

---
+++ alembic/versions/12130e23f2ef_add_connection_balance_equity.py
@@ -0,0 +1,23 @@
+"""add connection balance equity
+
+Revision ID: 12130e23f2ef
+Revises: ec154cd204fa
+Create Date: 2026-08-18 10:50:13.850276
+
+"""
+
+from typing import Sequence, Union
+
+
+revision: str = "12130e23f2ef"
+down_revision: Union[str, Sequence[str], None] = "ec154cd204fa"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
